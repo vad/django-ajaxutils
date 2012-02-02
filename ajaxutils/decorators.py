94b4b3bfd8b20aa839f27276a2786ab349627ccc
Davide Setti <davide.setti@gmail.com>
2012-02-02 14:49:44 +0100

    first commit

---
+++ ajaxutils/decorators.py
@@ -0,0 +1,58 @@
+from decorator import decorator
+
+try:
+    import simplejson as json
+except ImportError:
+    import json
+
+from django.http import HttpResponse
+
+
+
+class JsonResponse(HttpResponse):
+    """
+    HttpResponse descendant, which return response with ``application/json`` mimetype.
+    """
+    def __init__(self, data):
+        super(JsonResponse, self).__init__(content=json.dumps(data),
+                                           mimetype='application/json')
+
+
+
+def ajax(login_required=False, require_GET=False, require_POST=False):
+    """
+    Usage:
+
+    @ajax(login_required=True)
+    def my_ajax_view(request):
+        return {'count': 42}
+    """
+    def ajax(f, request, *args, **kwargs):
+        """ wrapper function """
+        if login_required:
+            if not request.user.is_authenticated():
+                return JsonResponse({
+                    'status': 'error',
+                    'error': 'Not authenticated',
+                })
+        if require_GET:
+            if request.method != 'GET':
+                return JsonResponse({
+                    'status': 'error',
+                    'error': 'Method not allowed',
+                })
+        if require_POST:
+            if request.method != 'POST':
+                return JsonResponse({
+                    'status': 'error',
+                    'error': 'Method not allowed',
+                })
+
+        response = f(request, *args, **kwargs)
+
+        if isinstance(response, dict):
+            return JsonResponse(response)
+        else:
+            return response
+
+    return decorator(ajax)
