f9cfa7e896d3791f15a137bd81519f71ea78ad78
Davide Setti <davide.setti@gmail.com>
2012-05-14 10:12:17 +0200

    added datetime objects serialization

--- ajaxutils/http.py
+++ ajaxutils/http.py
@@ -6,6 +6,16 @@ except ImportError:
 from django.http import HttpResponse
 
 
+def handler(obj):
+    if hasattr(obj, 'isoformat'):
+        return obj.isoformat()
+    else:
+        raise TypeError,\
+        'Object of type %s with value of %s is not JSON serializable' % (
+            type(obj), repr(obj)
+            )
+
+
 class JsonResponse(HttpResponse):
     """
     HttpResponse descendant, which return response with
@@ -14,7 +24,7 @@ class JsonResponse(HttpResponse):
 
     def __init__(self, data, mimetype='application/json', status=200):
         super(JsonResponse, self).__init__(
-            content=json.dumps(data),
+            content=json.dumps(data, default=handler),
             mimetype=mimetype,
             status=status
         )
